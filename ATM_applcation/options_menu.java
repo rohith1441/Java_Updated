3dc97633baac1093533da39642934693d61c6e85
rohithvalupadasu <rohithvalupadasu@gmail.com>
2023-09-14 11:29:39 -0500

    Added Exception handling for InputMisMatch

--- ATM_applcation/options_menu.java
+++ ATM_applcation/options_menu.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class options_menu {
     private double amount;
     private double saving_amount;
-    public void options(){
+    public void options() throws Exception {
         System.out.println("Select your Account:");
-        System.out.println("1 Checking Account"+"\n2 Saving Account"+"\n3 Exit");
-        System.out.print("Choice:");
+        System.out.println("1 Checking Account" + "\n2 Saving Account" + "\n3 Exit");
+        //System.out.print("Choice:");
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        if(choice==1){
+        int choice = 0;
+        //int count=1;
+        do{
+        try {
+            System.out.print("Choice:");
+            choice = scanner.nextInt();
+        } catch (Exception e) {
+            }
+        }while(choice==-1);
+        //scanner.nextLine();
+        choice = Integer.parseInt(String.valueOf(choice));
+        String select = String.valueOf(choice);
+        if ((select.equals("1"))) {
             CheckingAccount();
-        } else if (choice==2) SavingAccount();
-        else if (choice==3){
+        } else if (select.equals("2")) SavingAccount();
+        else if (select.equals("3")) {
             System.out.println("Thank You for using the application");
-        }
-        else{
+        } else {
             System.out.println("Enter the correct choice");
             options();
         }
+
     }
 
     public void setSaving_amount(double saving_amount) {
@@ -39,12 +50,19 @@ public class options_menu {
         return amount;
     }
 
-    public void CheckingAccount(){
+    public void CheckingAccount() throws Exception{
         System.out.println("Checking Account:");
         System.out.println("1 View Balance"+"\n2 Withdraw Funds"+"\n3 Deposit Funds"+"\n4 Exit");
         System.out.print("Choice: ");
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+        int choice=0;
+        do{
+        try{
+            System.out.print("Choice: ");
+            choice = scanner.nextInt();}catch (Exception e){
+            System.out.println("Enter the correct choice");
+        }
+        }while(choice==-1);
         if(choice==1){
             View_Balance();
         } else if (choice==2){
@@ -59,13 +77,13 @@ public class options_menu {
         }
 
     }
-    public void View_Balance(){
+    public void View_Balance() throws Exception {
         double money = getAmount();
         System.out.println("Your balance is:"+money+"$");
         options();
     }
 
-    public void WithDraw(){
+    public void WithDraw() throws Exception {
         System.out.println("Enter the amount you want to withdraw:");
         Scanner scanner = new Scanner(System.in);
         double with_amount = scanner.nextDouble();
@@ -79,7 +97,7 @@ public class options_menu {
         options();}
     }
 
-    public void Deposit(){
+    public void Deposit() throws Exception {
         System.out.println("Enter the amount you want to deposit:");
         Scanner scanner = new Scanner(System.in);
         double depo_amount = scanner.nextDouble();
@@ -95,21 +113,29 @@ public class options_menu {
 
     }
 
-    public void SavingAccount(){
+    public void SavingAccount() throws Exception {
         System.out.println("Saving Account: ");
-        System.out.println("1 View Balance"+"\n2 Withdraw Funds"+"\n3 Deposit Funds"+"\n4 Exit");
-        System.out.print("Choice: ");
+        System.out.println("1 View Balance" + "\n2 Withdraw Funds" + "\n3 Deposit Funds" + "\n4 Exit");
+
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        if(choice==1){
+        int choice = 0;
+        do{
+        try {
+            System.out.print("Choice: ");
+            choice = scanner.nextInt();
+        } catch (Exception e) {
+            //scanner.nextInt();
+            System.out.println("Enter the correct choice");
+        }}while (choice==-1);
+
+        if (choice == 1) {
             View_Balance_saving();
-        } else if (choice==2){
-            WithDraw_saving();}
-        else if (choice==3) Deposit_saving();
-        else if (choice==4){
+        } else if (choice == 2) {
+            WithDraw_saving();
+        } else if (choice == 3) Deposit_saving();
+        else if (choice == 4) {
             System.out.println("Thank You for using the application");
-        }
-        else{
+        } else {
             System.out.println("Enter the correct choice");
             SavingAccount();
         }
@@ -119,7 +145,7 @@ public class options_menu {
         System.out.println("Your saving account balance is:"+saving_balance+"$");
     }
 
-    public void WithDraw_saving(){
+    public void WithDraw_saving() throws Exception {
         System.out.println("Enter the amount you want to withdraw:");
         Scanner scanner = new Scanner(System.in);
         double with_amount = scanner.nextDouble();
@@ -133,7 +159,7 @@ public class options_menu {
             options();
         }
     }
-    public void Deposit_saving() {
+    public void Deposit_saving() throws Exception {
         System.out.println("Enter the amount you want to Deposit:");
         Scanner scanner = new Scanner(System.in);
         double depo_amount = scanner.nextDouble();
